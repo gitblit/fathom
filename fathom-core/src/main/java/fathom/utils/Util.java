e2ee4b111104fe3a8a0d3bcc575f45556ee8e4da
James Moger <james.moger@gitblit.com>
2016-01-28 10:11:03 -0500

    Change Util.logSetting from DEBUG to INFO

--- fathom-core/src/main/java/fathom/utils/Util.java
+++ fathom-core/src/main/java/fathom/utils/Util.java
@@ -194,7 +194,7 @@ public class Util {
     }
 
     public static void logSetting(Logger log, String name, Object value) {
-        log.debug(Strings.padEnd(name, 32, '.') + (value == null ? "" : (name.equalsIgnoreCase("password") ? "<masked>" : value.toString())));
+        log.info(Strings.padEnd(name, 32, '.') + (value == null ? "" : (name.equalsIgnoreCase("password") ? "<masked>" : value.toString())));
     }
 
     public static String getPreSubstring(String input, char c) {
